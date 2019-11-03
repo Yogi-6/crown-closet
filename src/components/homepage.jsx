e05e42e7109971b6240bbfb037aece9cb4ecb36a
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2019-11-03 14:39:58 +0530

    Style HomePage and Components

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,1 +1,15 @@
 import React, { Component } from "react";
+import Directory from "./common/directory/directory";
+import "../styles/homepage.scss";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="homepage">
+        <Directory />
+      </div>
+    );
+  }
+}
+
+export default HomePage;
